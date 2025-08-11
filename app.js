81e700ee53968a9a3ba184b7175925bca0f9619f
Jose Lara-Ceja <laracejaj@gmail.com>
2025-08-11 15:36:24 -0700

    created routes and added functionality to index

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+const mainRouter = require('./routes/index');
+const clothingItemsRoutes = require('./routes/clothingitems');
 const {  PORT = 3001 } = process.env;
 
-mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db');
+mongoose
+.connect('mongodb://127.0.0.1:27017/wtwr_db')
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch(console.error);
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+app.use('/', mainRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on ${PORT}`);
 });
-
