255613364495e24dd33ef8528729e335d0473c89
Jose Lara-Ceja <laracejaj@gmail.com>
2025-08-11 15:47:02 -0700

    finished with routes for the users and is functional

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const routers = require('express').Router();
 
-routers.get("/users", () => console.log("Get all users"));
-routers.get("/users/:userId", () => console.log("Get user by ID"));
-routers.post("/users", () => console.log("Create a new user"));
+routers.get("/", () => console.log("Get all users"));
+routers.get("/:userId", () => console.log("Get user by ID"));
+routers.post("/", () => console.log("Create a new user"));
 
 module.exports = routers;
