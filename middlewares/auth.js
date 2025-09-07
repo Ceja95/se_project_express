15af56970737c88bdd3901133f5f7fb9c8586225
Jose Lara-Ceja <laracejaj@gmail.com>
2025-09-06 17:39:15 -0700

    created a auth.js file inside of middlewares with token authorization

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,1 +1,19 @@
-//left off on part 5 of this project.
+const token = authorization.replace("Bearer ", "");
+const UNAUTHORIZED_ERROR_CODE = "./../utils/errors";
+
+payload = jwt.verify(token, JWT_SECRET);
+
+const publicRoutes = [
+  { method: "POST", path: "/signin" },
+  { method: "POST", path: "/signup" },
+  { method: "GET", path: "/items" },
+];
+
+try {
+  req.user = payload;
+  next();
+} catch (err) {
+  return res
+    .status(UNAUTHORIZED_ERROR_CODE)
+    .json({ message: "Invalid or expired token" });
+}
