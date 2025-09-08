f1be4a521bd48b9dade22e230ff1be0058ca325c
Jose Lara-Ceja <laracejaj@gmail.com>
2025-09-08 09:57:34 -0700

    added routers to the index.js post routers

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 const usersRouter = require('./users');
 const clothingItemsRouter = require('./clothingitems');
+const { createUser, login } = require('../controllers/users');
 
 router.use('/users', usersRouter);
 router.use('/items', clothingItemsRouter);
+router.post("/signup", createUser);
+router.post("/signin", login);
 
 router.use((req, res) => {
   res.status(404).send({ message: "An error has occurred on the server" });
