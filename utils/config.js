b3bdc000648a8546e1ed1612a0e0e8c8fdbc8eb5
Jose Lara-Ceja <laracejaj@gmail.com>
2025-12-14 21:31:39 -0800

    updated code

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,4 @@
-const { JWT_SECRET = "super-strong-secrect" } = process.env;
+const { JWT_SECRET = "ccb85e95864a87184e92b862816eabfd46943586bb92b89a4624e613e874b759" } = process.env;
 
 module.exports = {
   JWT_SECRET,
